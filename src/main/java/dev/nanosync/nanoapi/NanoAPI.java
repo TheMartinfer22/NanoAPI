bcb9b9d5d4e01e8f7c8e4c96595ecae799d14d65
Martin Hartfelder <martinhartfelder@mx2.unisc.br>
2022-01-12 09:46:43 -0300

    test scopes

--- src/main/java/dev/nanosync/nanoapi/NanoAPI.java
+++ src/main/java/dev/nanosync/nanoapi/NanoAPI.java
@@ -1,12 +1,20 @@
 package dev.nanosync.nanoapi;
 
 import dev.nanosync.nanoapi.entity.NanoPlayer;
+import dev.nanosync.nanoapi.external.DiscordConnector;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class NanoAPI extends JavaPlugin {
     @Override
     public void onEnable() {
-        new NanoPlayer(Bukkit.getPlayer("TheMartinfer22")).sendMessage("teste");
+        // NanoPlayer Examples
+        Player player = Bukkit.getPlayer("TheMartinfer22");
+        new NanoPlayer(player).sendMessage("teste");
+
+        // Discord Examples
+        DiscordConnector discord = new DiscordConnector("token", "121212");
+        discord.getGuild().getTextChannelById("1212").sendMessage("teste").queue();
     }
 }
