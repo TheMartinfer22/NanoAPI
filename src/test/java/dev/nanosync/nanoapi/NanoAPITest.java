64cf50d376fb450867eb00bb498a296622c0e117
Martinfer22 <martinsocial1706@gmail.com>
2022-02-05 19:46:18 -0300

    mock test

--- src/test/java/dev/nanosync/nanoapi/NanoAPITest.java
+++ src/test/java/dev/nanosync/nanoapi/NanoAPITest.java
@@ -2,24 +2,19 @@ package dev.nanosync.nanoapi;
 
 import be.seeseemelk.mockbukkit.MockBukkit;
 import be.seeseemelk.mockbukkit.ServerMock;
-import org.junit.After;
-import org.junit.Before;
 
 public class NanoAPITest {
 
     private static ServerMock server;
 
-    public static void setUp() {
+    public static void setup() {
         server = MockBukkit.mock();
         MockBukkit.load(NanoAPI.class);
         server.addPlayer("Dev");
         server.addPlayer("Martin");
     }
 
-    public static void tearDown() {
+    public static void shutdown() {
         MockBukkit.unmock();
     }
-
-
-
 }
